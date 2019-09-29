49859a650c1343baaa244462a655909590e8c977
tanfly <mcewen.stella@gmail.com>
2019-09-29 18:37:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ParentComponent from './components/ParentComponent';
+ 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <div>
+    <ParentComponent/>
+  </div>,
   document.getElementById('root')
 );
